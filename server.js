cdd3826f72394a5567b0fafb658d770607a206b2
Vincent7k <nicckamorim@gmail.com>
2024-04-11 10:12:41 -0300

    chore: crete routes

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import http from 'http';
 
+const PORT = 3000;
+
+const routes = {
+    "/": "Node.js course"
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-type': 'text/plain'});
-    res.end('Node.js course');
+    res.end(routes[req.url]);
 });
 
-    server.listen(3000, () =>{
+    server.listen(PORT, () =>{
         console.log('Hello, world!')
     })
