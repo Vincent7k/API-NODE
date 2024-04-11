0a167d171cb18bbffbb7a165fb6b48c7c5e53281
Vincent7k <nicckamorim@gmail.com>
2024-04-11 11:56:21 -0300

    create: update

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-import http from 'http';
-
+// import http from 'http';
+import app from './src/app.js';
 const PORT = 3000;
 
-const routes = {
-    "/": "Node.js course",
-    '/dogs': 'Entered in the dogs routes',
-    '/cats': 'Entered in the cats routes',
-};
+// const routes = {
+//     "/": "Node.js course",
+//     '/dogs': 'Entered in the dogs routes',
+//     '/cats': 'Entered in the cats routes',
+// };
 
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type': 'text/plain'});
-    res.end(routes[req.url]);
-});
+// const server = http.createServer((req, res) => {
+//     res.writeHead(200, {'Content-type': 'text/plain'});
+//     res.end(routes[req.url]);
+// });
 
-    server.listen(PORT, () =>{
+    app.listen(PORT, () =>{
         console.log('server OK')
     })
